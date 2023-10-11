fae833e7a050af094e7e9e0dc87f1528681c6808
Patrick Pinto <patrick.cb3@gmail.com>
2023-10-11 14:56:15 +0100

    Added specific rules for different urls

--- lib/devise_two_factor/strategies/two_factor_authenticatable.rb
+++ lib/devise_two_factor/strategies/two_factor_authenticatable.rb
@@ -4,6 +4,13 @@ module Devise
 
       def authenticate!
         resource = mapping.to.find_for_database_authentication(authentication_hash)
+
+        # ignores otp when requesting pre_authenticate
+        resource&.otp_required_for_login = false if path_is?('/pre_authenticate')
+
+        # forces otp when requesting activating otp for the first time
+        resource&.otp_required_for_login = true if path_is?('/activate_otp_and_sign_in')
+
         # We authenticate in two cases:
         # 1. The password and the OTP are correct
         # 2. The password is correct, and OTP is not required for login
@@ -17,12 +24,21 @@ module Devise
         # We want to cascade to the next strategy if this one fails,
         # but database authenticatable automatically halts on a bad password
         @halted = false if @result == :failure
+
+        # saves the resource if login was successfull
+        resource&.save if path_is?('/activate_otp_and_sign_in') && @result != :failure
       end
 
       def validate_otp(resource)
+        resource&.otp_required_for_login = true if path_is?('') && resource.force_otp?
+
         return true unless resource.otp_required_for_login
         return if params[scope]['otp_attempt'].nil?
         resource.validate_and_consume_otp!(params[scope]['otp_attempt'])
+      end
+
+      def path_is?(path)
+        request.path == "/users/session#{path}"
       end
     end
   end
